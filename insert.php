dbe2464c0cebf26b21aabe754f6f4d2973d2831a
sulindawod <suli0023@edu.zealand.dk>
2023-10-02 13:09:22 +0200

    Initial commit

---
+++ insert.php
@@ -0,0 +1,58 @@
+<?php
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Indsæt til database </title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/styles1.css" rel="stylesheet" type="text/css">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body
+
+<div class="container">
+    <form action="webshop.php" method="POST">
+
+        <label for="Navn">Navn:</label>
+        <input type="text" name="Navn" required><br>
+
+        <label for="Forfatter">Forfatter:</label>
+        <input type="text" name="Forfatter"><br>
+
+        <label for="udgivelsesdato">Udgivelsesdato:</label>
+        <input type="date" name="udgivelsesdato"><br>
+
+        <label for="Beskrivelse">Beskrivelse:</label>
+        <textarea name="Beskrivelse"></textarea><br>
+
+        <label for="pris">Pris:</label>
+        <input type="number" name="pris" step="0.01"><br>
+
+        <label for="Sidetal">Sidetal:</label>
+        <input type="number" name="Sidetal"><br>
+
+        <label for="genre">Genre:</label>
+        <input type="text" name="genre"><br>
+
+        <label for="bedømmelse">Bedømmelse:</label>
+        <input type="number" name="bedømmelse" min="1" max="5"><br>
+
+
+        <input type="submit" value="Tilføje bog">
+    </form>
+</div>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ settings/init.php
@@ -0,0 +1,18 @@
+<?php
+require "classes/classDB.php";
+
+define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
+
+if(CONFIG_LIVE == 0){
+    $DB_SERVER = "localhost";
+    $DB_NAME = "webshop";
+    $DB_USER = "root";
+    $DB_PASS = "root";
+}else{
+    $DB_SERVER = "";
+    $DB_NAME = "";
+    $DB_USER = "";
+    $DB_PASS = "";
+}
+
+$db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
